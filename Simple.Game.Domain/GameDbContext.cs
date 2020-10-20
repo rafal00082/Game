9327f54c390071b5b31e4bab117c95723cd4a1bf
rafalk82 <rafal00082@gmail.com>
2020-10-20 11:50:36 +0200

    add db configuration

---
+++ Simple.Game.Domain/GameDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple.Game.Domain
+{
+    public class GameDbContext : DbContext
+    {
+        public GameDbContext(DbContextOptions<GameDbContext> options)
+            : base(options)
+        { }
+    }
+}
