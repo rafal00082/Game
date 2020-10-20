5daee69acc2668655b447fff7bd939b3ea4ae45c
rafalk82 <rafal00082@gmail.com>
2020-10-20 12:10:38 +0200

    add swagger to project

--- Simble.Game.WebApi/Startup.cs
+++ Simble.Game.WebApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Simple.Game.Domain;
+using Microsoft.OpenApi.Models;
 
 namespace Simple.Game.WebApi
 {
@@ -23,6 +24,15 @@ namespace Simple.Game.WebApi
             services.AddDbContext<GameDbContext>(options =>
              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "WebApi for Simple.Game",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +52,12 @@ namespace Simple.Game.WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi V1");
             });
         }
     }
